3322b760567d3b9fed1e4608988b2db47fc1a779
Pei-ChunLee <34323644+Pei-ChunLee@users.noreply.github.com>
2019-03-13 15:00:22 -0700

    Emupay for NIC

---
+++ src/environments/prod.ts
@@ -0,0 +1,28 @@
+import { EnvironmentSchema } from './schema';
+
+/**
+ * Environment: prod
+ */
+const env: EnvironmentSchema = {
+  name: 'production',
+  enableAnimations: true,
+  ratesAPI: {
+    btc: 'https://bitpay.com/api/rates',
+    bch: 'https://bitpay.com/api/rates/bch'
+  },
+  activateScanner: true
+};
+
+/* When DefaultAsset defined here does exist in BWS db, Default Asset will be used to create default wallet
+   When DefaultAsset doesn't exist in BWS db but there are assets defined in BWS db and the first available asset
+   will be used as DefaultAsset to create wallet.
+   When there is no asset defined in BWS DB, BaseCoin will be used to create the default wallet.
+   When BaseCoin is not defined here, default wallet creation will fail
+ */
+
+// export const DefaultAsset = '9CAT';
+export const DefaultAsset = 'AIBT';
+export const BaseCoin = 'AIB';
+// export const DefaultWalletServiceUrl = "https://bws.9cat.in/bws/api";
+export const DefaultWalletServiceUrl = "http://91.121.91.168:43232/bws/api";
+export default env;
